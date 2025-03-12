b5f750338e5f4f13d398603ff9fc5a2a9c1dc169
karakaplanm <karakaplanm@gmail.com>
2025-03-12 23:19:19 +0300

    ek

--- work1/calitir.sh
+++ work1/calitir.sh
@@ -12,12 +12,12 @@ pymol 1AKI_processed.gro
 pymol 1AKI_solv.gro
 
 echo "Step 4: Add Ions for neutralize"
-wget http://www.mdtutorials.com/gmx/lysozyme/Files/ions.mdp
+#wget http://www.mdtutorials.com/gmx/lysozyme/Files/ions.mdp
 gmx grompp -f ions.mdp -c 1AKI_solv.gro -p topol.top -o ions.tpr
 echo "13" | gmx genion -s ions.tpr -o 1AKI_solv_ions.gro -p topol.top -pname NA -nname CL -neutral
 
 echo "Step 5: Energy Minimization"
-wget http://www.mdtutorials.com/gmx/lysozyme/Files/minim.mdp
+#wget http://www.mdtutorials.com/gmx/lysozyme/Files/minim.mdp
 gmx grompp -f minim.mdp -c 1AKI_solv_ions.gro -p topol.top -o em.tpr
 gmx mdrun -v -deffnm em
 
@@ -26,7 +26,7 @@ xmgrace potential.xvg
 
 echo "Step 6: Equilibration, Equilibration, Part 1"
 
-wget http://www.mdtutorials.com/gmx/lysozyme/Files/nvt.mdp
+#wget http://www.mdtutorials.com/gmx/lysozyme/Files/nvt.mdp
 gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
 gmx mdrun -deffnm nvt -nb gpu
 
@@ -35,7 +35,7 @@ xmgrace temperature.xvg
 
 
 echo "Step 7: Equilibration, Part 2"
-wget http://www.mdtutorials.com/gmx/lysozyme/Files/npt.mdp
+#wget http://www.mdtutorials.com/gmx/lysozyme/Files/npt.mdp
 gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
 gmx mdrun -deffnm npt -nb gpu
 
@@ -49,7 +49,7 @@ xmgrace density.xvg
 
 echo "Step 8: Production MD"
 
-wget http://www.mdtutorials.com/gmx/lysozyme/Files/md.mdp
+#wget http://www.mdtutorials.com/gmx/lysozyme/Files/md.mdp
 gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_1.tpr
 gmx mdrun -deffnm md_0_1 -nb gpu
 
@@ -58,7 +58,7 @@ echo "1 0 " | gmx trjconv -s md_0_1.tpr -f md_0_1.xtc -o md_0_1_noPBC.xtc -pbc m
 
 echo "4 4" | gmx rms -s md_0_1.tpr -f md_0_1_noPBC.xtc -o rmsd.xvg -tu ns
 xmgrace rmsd.xvg
-gmx rms -s em.tpr -f md_0_1_noPBC.xtc -o rmsd_xtal.xvg -tu ns
+echo "1 1" | gmx rms -s em.tpr -f md_0_1_noPBC.xtc -o rmsd_xtal.xvg -tu ns
 xmgrace rmsd_xtal.xvg
 
 echo "1" | gmx gyrate -s md_0_1.tpr -f md_0_1_noPBC.xtc -o gyrate.xvg
